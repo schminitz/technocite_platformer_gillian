8db6fbe6e5815e026da5ab3a8c800fe2e3fa7ee1
schminitz <gillian.sampont@gmail.com>
2019-11-07 12:13:14 +0100

    Add a spawner for player

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+	public Player playerToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Instantiate(playerToSpawn, transform.position, Quaternion.identity);
+    }
+
+}
