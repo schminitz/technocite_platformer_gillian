df1b6022f22cdc038cc0a4eb7c07664635f4c3a6
schminitz <gillian.sampont@gmail.com>
2019-11-15 11:07:57 +0100

    New main menu + navigate in first level of the menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+	public List<MenuItem> menuItems;
+	public Color activeColor;
+	public Color inactiveColor;
+
+	MenuItem activeMenuItem;
+	int activeIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		InitMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		MenuNavigation();
+    }
+
+	void MenuNavigation()
+	{
+		if (Input.GetKeyDown(KeyCode.DownArrow) ||
+			Input.GetKeyDown(KeyCode.S))
+		{
+			NavigateUpDown(goDown: true);
+		}
+		else if(Input.GetKeyDown(KeyCode.UpArrow) ||
+			    Input.GetKeyDown(KeyCode.Z))
+		{
+			NavigateUpDown(goDown: false);
+		}
+	}
+
+	void NavigateUpDown(bool goDown)
+	{
+		if (goDown)
+			activeIndex++;
+		else
+			activeIndex--;
+
+		activeIndex = (int)Mathf.Repeat(activeIndex, menuItems.Count);
+
+		activeMenuItem.SetInactive(inactiveColor);
+		activeMenuItem = menuItems[activeIndex];
+		activeMenuItem.SetActive(activeColor);
+	}
+
+	void InitMenu()
+	{
+		foreach (MenuItem menuItem in menuItems)
+		{
+			TextMesh textMesh = menuItem.GetComponent<TextMesh>();
+			textMesh.text = menuItem.label;
+			menuItem.SetInactive(inactiveColor);
+		}
+
+		activeIndex = 0;
+		activeMenuItem = menuItems[0];
+		activeMenuItem.SetActive(activeColor);
+	}
+}
---
+++ Assets/Scripts/MenuItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MenuAction
+{
+	GoDeeper,
+	Credits,
+	Quit,
+	NewGame,
+	Continue,
+	Resolution,
+	Fullscreen
+}
+
+public class MenuItem : MonoBehaviour
+{
+	public string label;
+	public MenuAction action;
+	public List<MenuItem> subMenuItems;
+
+	TextMesh textMesh;
+
+	// Start is called before the first frame update
+	void Awake()
+    {
+		textMesh = GetComponent<TextMesh>();
+    }
+
+	public void SetActive(Color color)
+	{
+		textMesh.color = color;
+	}
+
+	public void SetInactive(Color color)
+	{
+		textMesh.color = color;
+	}
+}
